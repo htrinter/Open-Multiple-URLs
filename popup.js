6036accdf52307c1dc3ee8fbb820d7719f825063
htrinter <htdev@posteo.de>
2019-10-05 13:15:05 +0200

    Migrate to ECMAScript 6

--- popup.js
+++ popup.js
@@ -1,53 +1,53 @@
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener('DOMContentLoaded', init);
 
-var cacheKey = 'open-mul-url-cache'
-var txtArea = document.getElementById('urls')
-var lazyLoadCheckbox = document.getElementById('lazyLoad')
-var randomCheckbox = document.getElementById('random')
+let cacheKey = 'open-mul-url-cache';
+let txtArea = document.getElementById('urls');
+let lazyLoadCheckbox = document.getElementById('lazyLoad');
+let randomCheckbox = document.getElementById('random');
 
 function init () {
   // read cached opts
-  var oldOpts = getCachedOpts()
-  txtArea.value = oldOpts.txt
-  lazyLoadCheckbox.checked = oldOpts.lazyLoad
-  randomCheckbox.checked = oldOpts.random
+  let oldOpts = getCachedOpts();
+  txtArea.value = oldOpts.txt;
+  lazyLoadCheckbox.checked = oldOpts.lazyLoad;
+  randomCheckbox.checked = oldOpts.random;
 
   // add event listener for buttons
-  document.getElementById('open').addEventListener('click', loadSites)
-  document.getElementById('extract').addEventListener('click', extractURLs)
+  document.getElementById('open').addEventListener('click', loadSites);
+  document.getElementById('extract').addEventListener('click', extractURLs);
 
   // record on state change
-  txtArea.addEventListener('change', function (e) {
-    recordOpts({ txt: e.target.value })
-  })
-  lazyLoadCheckbox.addEventListener('change', function () {
-    recordOpts({ lazyLoad: this.checked })
-  })
-  randomCheckbox.addEventListener('change', function () {
-    recordOpts({ random: this.checked })
-  })
+  txtArea.addEventListener('change', (e) => {
+    recordOpts({ txt: e.target.value });
+  });
+  lazyLoadCheckbox.addEventListener('change',() => {
+    recordOpts({ lazyLoad: this.checked });
+  });
+  randomCheckbox.addEventListener('change', () => {
+    recordOpts({ random: this.checked });
+  });
 
   // select text in form field
-  txtArea.select()
+  txtArea.select();
 }
 
 // get options from localStorage
 function getCachedOpts () {
-  let cache = localStorage.getItem(cacheKey)
+  let cache = localStorage.getItem(cacheKey);
   if (cache) {
-    return JSON.parse(cache)
+    return JSON.parse(cache);
   } else {
-    return { txt: '', lazyLoad: false, random: false }
+    return { txt: '', lazyLoad: false, random: false };
   }
 }
 
 // record options into localStorage
 function recordOpts (newOpt) {
-  var oldOpt = getCachedOpts()
+  let oldOpt = getCachedOpts();
   localStorage.setItem(
     cacheKey,
     JSON.stringify(Object.assign({}, oldOpt, newOpt))
-  )
+  );
 }
 
 /**
@@ -55,44 +55,44 @@ function recordOpts (newOpt) {
  * @param {Array} a items An array containing the items.
  */
 function shuffle (a) {
-  var j, x, i
+  let j, x, i;
   for (i = a.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1))
-    x = a[i]
-    a[i] = a[j]
-    a[j] = x
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
   }
-  return a
+  return a;
 }
 
 // load sites in new background tabs
 function loadSites (e) {
-  var urlschemes = ['http', 'https', 'file', 'view-source']
-  var urls = txtArea.value.split(/\r\n?|\n/g)
-  var lazyloading = lazyLoadCheckbox.checked
-  var random = randomCheckbox.checked
+  let urlschemes = ['http', 'https', 'file', 'view-source'];
+  let urls = txtArea.value.split(/\r\n?|\n/g);
+  let lazyloading = lazyLoadCheckbox.checked;
+  let random = randomCheckbox.checked;
 
   if (random) {
-    urls = shuffle(urls)
+    urls = shuffle(urls);
   }
 
-  for (var i = 0; i < urls.length; i++) {
-    theurl = urls[i].trim()
-    if (theurl != '') {
-      if (urlschemes.indexOf(theurl.split(':')[0]) == -1) {
-        theurl = 'http://' + theurl
+  for (let i = 0; i < urls.length; i++) {
+    let theurl = urls[i].trim();
+    if (theurl !== '') {
+      if (urlschemes.indexOf(theurl.split(':')[0]) === -1) {
+        theurl = 'http://' + theurl;
       }
       if (
         lazyloading &&
-        theurl.split(':')[0] != 'view-source' &&
-        theurl.split(':')[0] != 'file'
+        theurl.split(':')[0] !== 'view-source' &&
+        theurl.split(':')[0] !== 'file'
       ) {
         chrome.tabs.create({
           url: chrome.extension.getURL('lazyloading.html#') + theurl,
           selected: false
-        })
+        });
       } else {
-        chrome.tabs.create({ url: theurl, selected: false })
+        chrome.tabs.create({ url: theurl, selected: false });
       }
     }
   }
@@ -100,16 +100,16 @@ function loadSites (e) {
 
 // extract urls from text
 function extractURLs (e) {
-  var text = txtArea.value
+  let text = txtArea.value;
 
-  var urls = ''
-  var urlmatcharr
-  var urlregex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi
+  let urls = '';
+  let urlmatcharr;
+  let urlregex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi;
   while ((urlmatcharr = urlregex.exec(text)) !== null) {
-    var match = urlmatcharr[0]
-    urls += match + '\n'
+    let match = urlmatcharr[0];
+    urls += match + '\n';
   }
 
-  txtArea.value = urls
-  recordOpts({ txt: urls })
+  txtArea.value = urls;
+  recordOpts({ txt: urls });
 }
