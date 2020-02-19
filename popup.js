f976f2e1b6a8f7c4cefb9e37415f2c36094747f7
jtag cat <git@c7.ee>
2020-02-19 03:18:44 +0200

    Rework code and add ability to launch in new window.

--- popup.js
+++ popup.js
@@ -4,45 +4,52 @@ let cacheKey = 'open-mul-url-cache';
 let txtArea = document.getElementById('urls');
 let lazyLoadCheckbox = document.getElementById('lazyLoad');
 let randomCheckbox = document.getElementById('random');
+let newWindowCheckbox = document.getElementById('newWindow');
+let urlFromTextCheckbox = document.getElementById('urlFromText');
 
-function init () {
+function init() {
   // read cached opts
   let oldOpts = getCachedOpts();
   txtArea.value = oldOpts.txt;
   lazyLoadCheckbox.checked = oldOpts.lazyLoad;
   randomCheckbox.checked = oldOpts.random;
-
+  newWindowCheckbox.checked = oldOpts.newWindow
+  urlFromTextCheckbox.checked = oldOpts.urlFromText
   // add event listener for buttons
   document.getElementById('open').addEventListener('click', loadSites);
-  document.getElementById('extract').addEventListener('click', extractURLs);
 
   // record on state change
   txtArea.addEventListener('change', (e) => {
     recordOpts({ txt: e.target.value });
   });
-  lazyLoadCheckbox.addEventListener('change',() => {
+  lazyLoadCheckbox.addEventListener('change', () => {
     recordOpts({ lazyLoad: this.checked });
   });
   randomCheckbox.addEventListener('change', () => {
     recordOpts({ random: this.checked });
   });
-
+  newWindowCheckbox.addEventListener('change', () => {
+    recordOpts({ newWindow: this.checked });
+  });
+  urlFromTextCheckbox.addEventListener('change', () => {
+    recordOpts({ urlFromText: this.checked });
+  });
   // select text in form field
   txtArea.select();
 }
 
 // get options from localStorage
-function getCachedOpts () {
+function getCachedOpts() {
   let cache = localStorage.getItem(cacheKey);
   if (cache) {
     return JSON.parse(cache);
   } else {
-    return { txt: '', lazyLoad: false, random: false };
+    return { txt: '', lazyLoad: false, random: false, newWindow: false, urlFromText: false };
   }
 }
 
 // record options into localStorage
-function recordOpts (newOpt) {
+function recordOpts(newOpt) {
   let oldOpt = getCachedOpts();
   localStorage.setItem(
     cacheKey,
@@ -54,7 +61,7 @@ function recordOpts (newOpt) {
  * Shuffles array in place.
  * @param {Array} a items An array containing the items.
  */
-function shuffle (a) {
+function shuffle(a) {
   let j, x, i;
   for (i = a.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
@@ -64,45 +71,8 @@ function shuffle (a) {
   }
   return a;
 }
-
-// load sites in new background tabs
-function loadSites (e) {
-  let urlschemes = ['http', 'https', 'file', 'view-source'];
-  let urls = txtArea.value.split(/\r\n?|\n/g);
-  let lazyloading = lazyLoadCheckbox.checked;
-  let random = randomCheckbox.checked;
-
-  if (random) {
-    urls = shuffle(urls);
-  }
-
-  for (let i = 0; i < urls.length; i++) {
-    let theurl = urls[i].trim();
-    if (theurl !== '') {
-      if (urlschemes.indexOf(theurl.split(':')[0]) === -1) {
-        theurl = 'http://' + theurl;
-      }
-      if (
-        lazyloading &&
-        theurl.split(':')[0] !== 'view-source' &&
-        theurl.split(':')[0] !== 'file'
-      ) {
-        chrome.tabs.create({
-          url: chrome.extension.getURL('lazyloading.html#') + theurl,
-          active: false
-        });
-      } else {
-        chrome.tabs.create({
-          url: theurl,
-          active: false
-        });
-      }
-    }
-  }
-}
-
 // extract urls from text
-function extractURLs (e) {
+function extractURLs(e) {
   let text = txtArea.value;
 
   let urls = '';
@@ -115,4 +85,71 @@ function extractURLs (e) {
 
   txtArea.value = urls;
   recordOpts({ txt: urls });
+}
+
+// load sites in new background tabs
+function loadSites(e) {
+  let urlschemes = ['http', 'https', 'file', 'view-source'];
+  let urlfromtext = urlFromTextCheckbox.checked
+  if (urlfromtext) {
+    extractURLs
+  }
+  let urls = txtArea.value.split(/\r\n?|\n/g);
+  let lazyloading = lazyLoadCheckbox.checked;
+  let random = randomCheckbox.checked;
+  let innewwindow = newWindowCheckbox.checked
+
+  if (random) {
+    urls = shuffle(urls);
+  }
+  var cleanUrls = []
+  for (var i = 0, len = urls.length; i < len; i++) {
+    if (urls[i] !== '') {
+      if (urlschemes.indexOf(urls[i].split(':')[0]) === -1) {
+        urls[i] = 'http://' + urls[i];
+      } // if is not url, make it an url
+      urls[i] = urls[i].trim(); // remove whitespace
+      cleanUrls.push(urls[i]);
+    };
+  }; // get legit urls
+  if (lazyloading) {
+    var compatibleUrls = [];
+    for (var i = 0, len = cleanUrls.length; i < len; i++) {
+      if (cleanUrls[i].split(':')[0] !== 'file' && cleanUrls[i].split(':')[0] !== 'file') {
+        compatibleUrls.push(urls[i]);
+      };
+    }; // remove urls not compatible with lazyloader
+    let lazyurls = []
+    compatibleUrls.map(function (a) {
+      lazyurls.push(
+        chrome.extension.getURL('lazyloading.html#') + a)
+    }); // pre-append lazyloading to all urls
+    if (innewwindow) {
+      chrome.windows.create({
+        url: lazyurls
+      }) // launch urls in new window 
+    } else {
+      for (let i = 0; i < urls.length; i++) {
+        let theurl = lazyurls[i] // get current url
+        chrome.tabs.create({
+          url: theurl,
+          active: false
+        }); // tabs go one by one
+      }
+    }
+  } else { // do not lazyload
+    if (innewwindow) {
+      chrome.windows.create({
+        url: cleanUrls
+      }); // in new window
+    } else {
+      for (let i = 0; i < urls.length; i++) {
+        let theurl = cleanUrls[i] // get current url
+        chrome.tabs.create({
+          url: theurl,
+          active: false
+        }); // tabs go one by one
+      }
+    }
+  }
 }
