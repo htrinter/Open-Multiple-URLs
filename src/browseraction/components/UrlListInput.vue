48547a0c59d3c06fed3d324f05f273f45bfca43d
htrinter <htdev@posteo.de>
2023-11-21 21:37:15 +0100

    add auto-select behaviour

--- src/browseraction/components/UrlListInput.vue
+++ src/browseraction/components/UrlListInput.vue
@@ -1,15 +1,11 @@
-<script lang="ts" setup>
-import { store } from '@/browseraction/components/store/store'
-</script>
-
 <template>
   <section id="url-list-input">
     <label for="urls">List of URLs / Text to extract URLs from:</label>
     <textarea
+      ref="urlTextArea"
       id="urls"
       wrap="soft"
       tabindex="1"
-      autofocus
       :value="store.urlList"
       @input="handleUrlListInput"
     ></textarea>
@@ -17,7 +13,28 @@ import { store } from '@/browseraction/components/store/store'
 </template>
 
 <script lang="ts">
+import { onMounted, ref } from 'vue'
+import { store } from '@/browseraction/components/store/store'
+
 export default {
+  computed: {
+    store() {
+      return store
+    }
+  },
+  setup() {
+    const urlTextArea = ref(null)
+
+    onMounted(() => {
+      if (urlTextArea.value) {
+        ;(urlTextArea.value as unknown as HTMLInputElement).select()
+      }
+    })
+
+    return {
+      urlTextArea
+    }
+  },
   methods: {
     handleUrlListInput(event: Event) {
       store.setUrlList((event?.target as HTMLInputElement).value)
