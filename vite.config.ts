6d2c9c48a1127c8e1d5c068444c4e633b0ea2b31
htrinter <htdev@posteo.de>
2023-11-29 19:28:04 +0100

    add custom plugin for setting manifest version on copy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,33 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import {viteStaticCopy} from "vite-plugin-static-copy";
+import * as path from 'path';
+import * as fs from 'fs';
+import {version} from './package.json'
 
-// https://vitejs.dev/config/
+const copyManifestWithVersion = () => {
+  return {
+    name: 'copy-manifest-with-version',
+    generateBundle() {
+      const target = process.env.TARGET
+      if (["firefox", "chrome"].indexOf(target ?? "") === -1) {
+        throw new Error("Please specify process.env.TARGET 'firefox' or 'chrome'.")
+      }
+
+      const manifestSourcePath = path.resolve(__dirname, `./src/manifest/${target}/manifest.json`)
+      const manifestContent = fs.readFileSync(manifestSourcePath, 'utf-8').replace('__VERSION__', String(version))
+
+      const manifestTargetDir =  path.resolve(__dirname, `./dist-${target}/`)
+      if (!fs.existsSync(manifestTargetDir)){
+        fs.mkdirSync(manifestTargetDir);
+      }
+
+      const manifestTargetPath = path.resolve(manifestTargetDir, "manifest.json")
+      fs.writeFileSync(manifestTargetPath, manifestContent, 'utf-8');
+    }
+  }
+};
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,20 +38,10 @@ export default defineConfig({
         {
           src: "src/icons/*.png",
           dest: ""
-        },
-        {
-          src: ["firefox", "chrome"]
-              .filter(browser => {
-                if (!process.env.TARGET || ["firefox", "chrome"].indexOf(process.env.TARGET) === -1) {
-                  throw new Error("Please specify process.env.TARGET 'firefox' or 'chrome'.")
-                }
-                return process.env.TARGET == browser
-              })
-              .map(browser => `src/manifest/${browser}/manifest.json`)[0],
-          dest: ""
         }
       ]
-    })
+    }),
+      copyManifestWithVersion()
   ],
   resolve: {
     alias: {
