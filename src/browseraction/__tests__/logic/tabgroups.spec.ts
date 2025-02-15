c1eb9d240ae96c813b58cc2dd1593df6fa086944
htrinter <htdev@posteo.de>
2025-02-15 17:18:03 +0100

    add test

--- src/browseraction/__tests__/logic/tabgroups.spec.ts
+++ src/browseraction/__tests__/logic/tabgroups.spec.ts
@@ -2,13 +2,16 @@ import { afterEach, describe, expect, it, vi } from 'vitest'
 import {
   loadTabGroups,
   NEW_TAB_GROUP_ID,
-  NO_TAB_GROUP_ID
+  NEW_TAB_GROUP_TITLE,
+  NO_TAB_GROUP_ID,
+  NO_TAB_GROUP_TITLE
 } from '@/browseraction/components/logic/tabgroups'
+import browser from 'webextension-polyfill'
 import { beforeEach } from 'node:test'
 
-const tg1 = { id: 1, title: 'Group 1', color: 'red' }
-const tg2 = { id: 2, title: 'Group 2', color: 'blue' }
-const tg3 = { id: 3, title: '', color: 'green' }
+const tg1: Partial<browser.TabGroups.TabGroup> = { id: 1, title: 'Group 1', color: 'red' }
+const tg2: Partial<browser.TabGroups.TabGroup> = { id: 2, title: 'Group 2', color: 'blue' }
+const tg3: Partial<browser.TabGroups.TabGroup> = { id: 3, title: '', color: 'green' }
 
 describe('loads tab groups', () => {
   beforeEach(() => {
@@ -29,8 +32,8 @@ describe('loads tab groups', () => {
     const tabGroups = await loadTabGroups()
 
     expect(tabGroups).toEqual([
-      { id: NO_TAB_GROUP_ID, title: 'No Tab Group' },
-      { id: NEW_TAB_GROUP_ID, title: 'New Tab Group' },
+      { id: NO_TAB_GROUP_ID, title: NO_TAB_GROUP_TITLE },
+      { id: NEW_TAB_GROUP_ID, title: NEW_TAB_GROUP_TITLE },
       { id: 1, title: 'Group 1 (red)' },
       { id: 2, title: 'Group 2 (blue)' },
       { id: 3, title: '(green)' }
